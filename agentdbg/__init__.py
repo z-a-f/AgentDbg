e804bb2c450020cb49010cbed02ce351d1e4187b
Zafar <z.tahirov@gmail.com>
2026-02-16 13:05:56 -0800

    v0.1.0: Adding versioning

--- agentdbg/__init__.py
+++ agentdbg/__init__.py
@@ -1,5 +1,6 @@
 """AgentDbg: local-first agent debugging (trace, record_llm_call, record_tool_call, record_state)."""
 
 from agentdbg.tracing import record_llm_call, record_state, record_tool_call, trace
+from agentdbg.version import version as __version__
 
-__all__ = ["trace", "record_llm_call", "record_tool_call", "record_state"]
+__all__ = ["trace", "record_llm_call", "record_tool_call", "record_state", "__version__"]
---
+++ agentdbg/version.py
@@ -0,0 +1,2 @@
+# This file defines the version across the project.
+version = "0.1.0"
